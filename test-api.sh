8b0a581701b93aab384776d323668adf2d02b2b2
GlamorousCar <YaBoG228holodec@gmail.com>
2024-12-09 00:02:22 +0300

    update README.md

--- test-api.sh
+++ test-api.sh
@@ -3,29 +3,22 @@
 # Store the API base URL
 API_URL="http://localhost:8080"
 
-echo "Тестирование зарпосов к 1 практике"
+echo -e "\n \n ======Тестирование запросов по 1 практике (CRUD операции)======\n"
 
-## First, let's get a JWT token (assuming we have an endpoint that generates tokens)
-#TOKEN=$(curl -s -X POST "${API_URL}/auth/token" \
-#  -H "Content-Type: application/json" \
-#  -d '{"username": "test", "password": "test"}' | jq -r '.token')
-#
-#echo "JWT Token: $TOKEN"
 
-# GET all books
-echo -e "\nGetting all books:"
+echo -e "\nПолучение всех книг :\n"
 curl -s -X GET "${API_URL}/books"
-#\
-#  -H "Authorization: Bearer $TOKEN" | jq '.'
+sleep 2
 
-# GET a specific book
-echo -e "\nGetting book with ID 1:"
+echo -e "\n Получение книги по id 1:\n"
 curl -s -X GET "${API_URL}/books/1"
-#\
-#  -H "Authorization: Bearer $TOKEN" | jq '.'
+sleep 2
 
-# POST a new book
-echo -e "\nCreating a new book:"
+echo -e "\nСоздание новой записи книги:\n Запись {
+                             \"id\": \"5\",
+                             \"title\": \"The Pragmatic Programmer\",
+                             \"author\": \"Andy Hunt\"
+                                            }\":"
 curl -s -X POST "${API_URL}/books" \
   -H "Content-Type: application/json" \
   -d '{
@@ -33,28 +26,29 @@ curl -s -X POST "${API_URL}/books" \
       "title": "The Pragmatic Programmer",
       "author": "Andy Hunt"
     }'
-#    | jq '.'
-#    \
-#  -H "Authorization: Bearer $TOKEN" \
-# DELETE a book
-echo -e "\nDeleting book with ID 5:"
-curl -s -X DELETE "${API_URL}/books/5" \
- -H "Authorization: Bearer $TOKEN"
-
-# Verify deletion by getting all books again
-echo -e "\nVerifying deletion - getting all books:"
+sleep 2
+echo -e "\nПроверка наличия книги по ID 5 перед удалением:\n"
 curl -s -X GET "${API_URL}/books"
+sleep 2
+
+echo -e "\nУдаление книги по ID 5:"
+curl -s -X DELETE "${API_URL}/books/5"
+sleep 2
+# Verify deletion by getting all books again
+echo -e "\nПодтверждение удаления книги: - получение всех книг::\n"
+curl -s -X GET "${API_URL}/books"
+sleep 2
 #\
 #  -H "Authorization: Bearer $TOKEN" | jq '.'
 
 # PUT (update) a book
-echo -e "\nUpdating book with ID 4:"
-curl -s -X PUT "${API_URL}/books/4" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
+echo -e "\nОбновление книги по ID 3:"
+curl -s -X PUT "${API_URL}/books/3" \
   -d '{
     "title": "The Pragmatic Programmer: 20th Anniversary Edition",
     "author": "Andy Hunt and Dave Thomas"
   }'
-#   | jq '.'
-
+sleep 2
+echo -e "\nПроверка j,обновления:\n"
+curl -s -X GET "${API_URL}/books"
+sleep 2
